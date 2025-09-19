6021b3d71585f294f1f6f164a6ea0c9bc3bd7066
aditya-raj-112 <braininsight9@gmail.com>
2025-09-20 00:37:06 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://3.80.143.36:84";
+const API_BASE_URL = "http://a28609c84d5ca4284acd1be13700a9f2-1727456689.us-east-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
